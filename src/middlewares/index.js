ff6dd6ce16609e8a973a35bc3e2a17e716de7ad8
Tahsin Haider <tahsin3194@gmail.com>
2025-12-20 22:01:58 +0500

    Update: add oauth with google, env mount as esm imports,

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,2 +1,3 @@
 export * from './error.middleware.js';
 export * from './auth.middleware.js';
+export * from './passport.middleware.js';
---
+++ src/middlewares/passport.middleware.js
@@ -0,0 +1,17 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `${process.env.BACKEND_URL || 'http://localhost:5000'}/api/auth/google/callback`,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      done(null, profile);
+    }
+  )
+);
+
+export { passport };
